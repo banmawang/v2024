7256079a6615e3c0cbb6b7b950da12b3fccc7627
banmashou <agcwhyinuo@sina.com>
2024-05-29 21:15:32 +0800

    自定义错误消息

--- bmcms/src/auth/dto/register.dto.ts
+++ bmcms/src/auth/dto/register.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, Length, Matches } from 'class-validator'
 
 export class RegisterDto {
   @IsNotEmpty({ message: '用户名不能为空' })
+  @Matches(/^[a-z]$/i, { message: '请输入英文字母' })
+  @Length(3, 10, { message: '用户名长度为3-10个字符' })
   name: string
   @IsNotEmpty({ message: '密码不能为空' })
+  @Length(3, 20, { message: '密码长度为3-10个字符' })
   password: string
 }
