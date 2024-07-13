66e321ea19233e8beb8cebfc119d5e1cb909f936
banmashou <agcwhyinuo@sina.com>
2024-07-13 13:31:07 +0800

    使用iconpark定义配置项的图标

--- snippets/src/renderer/src/components/Search/index.tsx
+++ snippets/src/renderer/src/components/Search/index.tsx
@@ -1,10 +1,19 @@
+import { SettingOne } from '@icon-park/react'
 import useSearch from '@renderer/hooks/useSearch'
 
 export default function Search(): JSX.Element {
   const { search, handleSearch } = useSearch()
   return (
     <div className="bg-slate-50 p-3 rounded-lg drag">
-      <section className="bg-slate-200 p-3 rounded-lg">
+      <section className="bg-slate-200 p-3 rounded-lg flex items-center gap-1 nodrag">
+        <SettingOne
+          theme="outline"
+          size="22"
+          fill="#34495e"
+          strokeWidth={4}
+          className="cursor-pointer"
+          onClick={() => alert('显示配置页面')}
+        />
         <input
           value={search}
           onChange={handleSearch}
