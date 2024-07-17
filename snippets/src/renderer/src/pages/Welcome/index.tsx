38f3d4f3fe328d74633c40b49f164ef83936dd4b
banmawang <banmawang2021@163.com>
2024-07-17 11:30:32 +0800

    添加默认欢迎页面

---
+++ snippets/src/renderer/src/pages/Welcome/index.tsx
@@ -0,0 +1,8 @@
+export const Welcome = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-sm text-slate-600 opacity-80">
+      {/* <Code theme="outline" size="48" fill="#0a0707cd" strokeWidth={3} /> */}
+      欢迎使用斑马兽代码片段工具
+    </div>
+  )
+}
