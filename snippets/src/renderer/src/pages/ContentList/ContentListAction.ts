7933b0bb5b601f86244a985c055f3a39d5455269
banmawang <banmawang2021@163.com>
2024-07-17 15:35:00 +0800

    完成片段删除操作

--- snippets/src/renderer/src/pages/ContentList/ContentListAction.ts
+++ snippets/src/renderer/src/pages/ContentList/ContentListAction.ts
@@ -3,13 +3,17 @@ import { redirect } from 'react-router-dom'
 export default async ({ request, params }) => {
   const cid = params.cid || 0
   const formData = await request.formData()
-  switch (formData.get('action')) {
-    case 'add': {
+  const data = Object.fromEntries(formData)
+  switch (request.method) {
+    case 'POST': {
       const id = await window.api.sql(
         `insert into contents (title,content,category_id,created_at) values('未命名片段','',${cid},dateTime())`,
         'insert'
       )
       return redirect(`content/${id}`)
+    }
+    case 'DELETE': {
+      return await window.api.sql(`delete from contents where id=${data.id}`, 'del')
     }
   }
   return {}
--- snippets/src/renderer/src/pages/ContentList/index.tsx
+++ snippets/src/renderer/src/pages/ContentList/index.tsx
@@ -1,9 +1,11 @@
-import { Add } from '@icon-park/react'
+import { Add, Delete } from '@icon-park/react'
 import dayjs from 'dayjs'
+import { useContextMenu } from 'mantine-contextmenu'
 import { Form, NavLink, Outlet, useLoaderData, useSubmit } from 'react-router-dom'
 import './contentList.scss'
 
 export const ContentList = () => {
+  const { showContextMenu } = useContextMenu()
   const contents = useLoaderData() as ContentType[]
   const submit = useSubmit()
   return (
@@ -26,7 +28,7 @@ export const ContentList = () => {
               fill="#000"
               strokeWidth={2}
               onClick={() => {
-                submit({ action: 'add' }, { method: 'POST' })
+                submit(null, { method: 'POST' })
               }}
             />
           </div>
@@ -36,10 +38,23 @@ export const ContentList = () => {
             to={`/config/category/contentList/${content.category_id}/content/${content.id}`}
             key={content.id}
             className="flex justify-between items-center"
+            onContextMenu={showContextMenu(
+              [
+                {
+                  key: 'remove',
+                  icon: <Delete theme="outline" size="18" strokeWidth={3} />,
+                  title: '删除片段',
+                  onClick: () => {
+                    submit({ id: content.id }, { method: 'DELETE' })
+                  }
+                }
+              ],
+              { className: 'contextMenu' }
+            )}
           >
             <div className="truncate">{content.title}</div>
             <div className="text-[10px] opacity-80">
-              {dayjs(content.created_at).format('YYYY-MM-DD')}
+              {dayjs(content.created_at).format('YY-MM-DD')}
             </div>
           </NavLink>
         ))}
